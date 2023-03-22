8418d3efcee50818896df6e4620e7338b552102d
Delaplace <franck.delaplace@gmail.com>
2023-03-22 20:38:56 +0100

    minor

--- chrocos.py
+++ chrocos.py
@@ -13,7 +13,7 @@
 #   - d is the number of nodes with the same color. 0<=d<=n
 # The colors are integers from 1 to r and 0 stands for the transparent color
 
-# ** Import functions & packages, typing ======================================================
+# ** Import functions & packages, typing ==================================================
 from math import factorial, comb, ceil, exp, inf
 from typing import TypeAlias, Callable
 from scipy.stats import gmean
